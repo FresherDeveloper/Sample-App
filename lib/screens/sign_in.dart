57eba04b4442f870d3422ad86ceeffee88bf2304
mariya@fidisys.com <mariya@fidisys.com>
2023-08-04 12:38:35 +0530

    textfield changed to textformfield to implement validation

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -16,7 +16,7 @@ class _SignInScreenState extends State<SignInScreen> {
   final TextEditingController emailController = TextEditingController();
 
   final TextEditingController passwordController = TextEditingController();
-
+  final _loginFormKey = GlobalKey<FormState>();
   bool isButtonVissible = true;
   void clearText() {
     emailController.clear();
@@ -49,95 +49,122 @@ class _SignInScreenState extends State<SignInScreen> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                
-                controller: emailController,
-                
-                decoration: const InputDecoration(
-                  labelText: 'Email',
+          child: Form(
+            key: _loginFormKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  onSaved: (newValue) {
+                    emailController.text = newValue!;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your Email';
+                    }
+                    return null;
+                  },
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                  ),
                 ),
-                    
-              ),
-              TextField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
+                TextFormField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                  ),
+                  onSaved: (newValue) {
+                    passwordController.text = newValue!;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your Password';
+                    }
+                    return null;
+                  },
+                  obscureText: true,
                 ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 20),
-              isButtonVissible
-                  ? ElevatedButton(
-                      onPressed: () {
-                        final email = emailController.text;
-                        final password = passwordController.text;
-                        dataProvider
-                            .signInWithEmailAndPassword(email, password)
-                            .then((user) async {
-                          if (user != null) {
-                            // Navigate to home or dashboard screen after successful login
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const HomePage(),
-                                ));
-                          } else {
-                            // Show an error message
-                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                content: Text(dataProvider.errorMessage)));
+                const SizedBox(height: 20),
+                isButtonVissible
+                    ? ElevatedButton(
+                        onPressed: () {
+                          final email = emailController.text;
+                          final password = passwordController.text;
+                          if (_loginFormKey.currentState!.validate()) {
+                            _loginFormKey.currentState!.save();
                           }
-                        });
-                      },
-                      child: const Text('Login'),
-                    )
-                  : const SizedBox(),
-              const SizedBox(height: 20),
-              !isButtonVissible
-                  ? const SizedBox()
-                  : Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text("Don't you have an account?"),
-                        TextButton(
-                            onPressed: () {
+                          dataProvider
+                              .signInWithEmailAndPassword(email, password)
+                              .then((user) async {
+                            if (user != null) {
+                              // Navigate to home or dashboard screen after successful login
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const HomePage(),
+                                  ));
+                            } else {
+                              // Show an error message
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      content:
+                                          Text(dataProvider.errorMessage)));
+                            }
+                          });
+                        },
+                        child: const Text('Login'),
+                      )
+                    : const SizedBox(),
+                const SizedBox(height: 20),
+                !isButtonVissible
+                    ? const SizedBox()
+                    : Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Don't you have an account?"),
+                          TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  isButtonVissible = false;
+                                });
+                              },
+                              child: const Text("SignUp"))
+                        ],
+                      ),
+                isButtonVissible
+                    ? const SizedBox()
+                    : ElevatedButton(
+                        onPressed: () {
+                          final email = emailController.text;
+                          final password = passwordController.text;
+                          if (_loginFormKey.currentState!.validate()) {
+                            _loginFormKey.currentState!.save();
+                          }
+                          dataProvider
+                              .registerWithEmailAndPassword(email, password)
+                              .then((user) {
+                            if (user != null) {
+                              // Registration successful, navigate back to login screen
+                              // Navigator.pop(context);
                               setState(() {
-                                isButtonVissible = false;
+                                isButtonVissible = true;
                               });
-                            },
-                            child: const Text("SignUp"))
-                      ],
-                    ),
-              isButtonVissible
-                  ? const SizedBox()
-                  : ElevatedButton(
-                      onPressed: () {
-                        final email = emailController.text;
-                        final password = passwordController.text;
-
-                        dataProvider
-                            .registerWithEmailAndPassword(email, password)
-                            .then((user) {
-                          if (user != null) {
-                            // Registration successful, navigate back to login screen
-                            // Navigator.pop(context);
-                            setState(() {
-                              isButtonVissible = true;
-                            });
-                            clearText();
-                          } else {
-                            // Show an error message
-                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                content: Text(dataProvider.errorMessage)));
-                            log("failed ");
-                          }
-                        });
-                      },
-                      child: const Text('Create an Account'),
-                    ),
-            ],
+                              clearText();
+                            } else {
+                              // Show an error message
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      content:
+                                          Text(dataProvider.errorMessage)));
+                              log("failed ");
+                            }
+                          });
+                        },
+                        child: const Text('Create an Account'),
+                      ),
+              ],
+            ),
           ),
         ),
       ),
